5dcc62813c8cfe68b0c0fb1f1e926f5266804956
christophberger <christophberger@users.noreply.github.com>
2017-10-25 21:20:38 +0200

    Rename grafana_test.go to dashboard_test.go. Make metrics type internal.

--- dashboard_test.go
+++ dashboard_test.go
@@ -5,44 +5,46 @@ import (
 	"testing"
 )
 
-func TestServer_CreateMetric(t *testing.T) {
+func TestDashboard_CreateMetric(t *testing.T) {
 	type args struct {
 		target string
 		size   int
 	}
 
-	metrics := &Metrics{sync.Mutex{}, map[string]*Metric{}}
+	mt := &metrics{sync.Mutex{}, map[string]*Metric{}}
 
 	tests := []struct {
 		name    string
-		Metrics *Metrics
+		Metrics *metrics
 		args    args
 		wantErr bool
 	}{
 		{
 			"create1",
-			metrics,
+			mt,
 			args{"target1", 1000},
 			false,
 		},
 		{
 			"create1again",
-			metrics,
+			mt,
 			args{"target1", 1000},
 			true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			srv := &Server{
-				Metrics: tt.Metrics,
+			srv := &Dashboard{
+				s: &server{
+					metrics: tt.Metrics,
+				},
 			}
 			got, err := srv.CreateMetric(tt.args.target, tt.args.size)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Server.CreateMetric() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			want := srv.Metrics.metric[tt.args.target]
+			want := d.s.metrics.metric[tt.args.target]
 			if got != want { // strict identity
 				t.Errorf("Server.CreateMetric() = %v, want %v", got, want)
 			}
