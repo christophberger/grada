a8d841ba1604a352bae460051dd928418233ee39
christophberger <christophberger@users.noreply.github.com>
2017-11-06 13:12:07 +0100

    Add BufSizeFor(). Add comments.

--- dashboard.go
+++ dashboard.go
@@ -1,10 +1,17 @@
 package grada
 
+import (
+	"time"
+)
+
 type Dashboard struct {
 	srv *server
 }
 
-// GetDashboard initializes and/or returns the only existing dashboard
+// GetDashboard initializes and/or returns the only existing dashboard.
+// This also starts the HTTP server that responds to queries from Grafana.
+// Default port is 3001. Overwrite this port by setting the environment
+// variable GRADA_PORT to the desired port number.
 func GetDashboard() *Dashboard {
 	d := &Dashboard{}
 	d.srv = startServer()
@@ -17,6 +24,17 @@ func GetDashboard() *Dashboard {
 // (which is rarely needed), call DeleteMetric first.
 func (d *Dashboard) CreateMetric(target string, size int) (*Metric, error) {
 	return d.srv.metrics.Create(target, size)
+}
+
+// BufSizeFor takes a duration and a rate (number of data points per second)
+// and returns the required ring buffer size.
+// BufSizeFor use with Metrics.Create() as follows:
+//         d.CreateMetric("mytarget", d.BufSizeFor(5* time.Minute, 10 * time.Second))
+func (d *Dashboard) BufSizeFor(timeRange, interval time.Duration) int {
+	if interval.Nanoseconds() >= timeRange.Nanoseconds() {
+		return 1
+	}
+	return int(timeRange.Nanoseconds() / interval.Nanoseconds())
 }
 
 // DeleteMetric deletes the metric for the given target from the server.
