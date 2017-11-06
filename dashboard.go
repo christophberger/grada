43935bedb2ce57791c85b79692630ab7f382fe01
christophberger <christophberger@users.noreply.github.com>
2017-11-06 16:11:59 +0100

    Rename CreateMetric to CreateMetricWIthBufSize. Add new CreateMetric method that takes a time range and a time interval and calculates the buffer size from that. Make bufSizeFor() internal.

--- dashboard.go
+++ dashboard.go
@@ -18,19 +18,44 @@ func GetDashboard() *Dashboard {
 	return d
 }
 
-// CreateMetric creates a new metric for the given target and with the
-// given buffer size, and stores this metric in the server.
+// CreateMetric creates a new metric for the given target, time range, and
+// data update interval, and stores this metric in the server.
+//
+// timeRange is the maximum time range the Grafana dashboard will ask for.
+// This depends on the user setting for the dashboard.
+//
+// interval is the (average) interval in which the data points get delivered.
+//
+// The quotient of timeRange and interval determines the size of the ring buffer
+// that holds the most recent data points.
+// Typically, the timeRange of a dashboard request should be much larger than
+// the interval for the incoming data.
+//
 // Creating a metric for an existing target is an error. To replace a metric
 // (which is rarely needed), call DeleteMetric first.
-func (d *Dashboard) CreateMetric(target string, size int) (*Metric, error) {
+func (d *Dashboard) CreateMetric(target string, timeRange, interval time.Duration) (*Metric, error) {
+	return d.CreateMetricWithBufSize(target, d.bufSizeFor(timeRange, interval))
+}
+
+// CreateMetricWithBufSize creates a new metric for the given target and with the
+// given buffer size, and stores this metric in the server.
+//
+// Buffer size should be chosen so that the buffer can hold enough items for a given
+// time range that Grafana asks for and the given rate of data point updates.
+//
+// Example: If the dashboards's time range is 5 minutes and the incoming data arrives every
+// second, the buffer should hold 300 item (5*60*1) at least.
+//
+// Creating a metric for an existing target is an error. To replace a metric
+// (which is rarely needed), call DeleteMetric first.
+func (d *Dashboard) CreateMetricWithBufSize(target string, size int) (*Metric, error) {
 	return d.srv.metrics.Create(target, size)
 }
 
-// BufSizeFor takes a duration and a rate (number of data points per second)
+// bufSizeFor takes a duration and a rate (number of data points per second)
 // and returns the required ring buffer size.
-// BufSizeFor use with Metrics.Create() as follows:
-//         d.CreateMetric("mytarget", d.BufSizeFor(5* time.Minute, 10 * time.Second))
-func (d *Dashboard) BufSizeFor(timeRange, interval time.Duration) int {
+// Used by CreateMetric().
+func (d *Dashboard) bufSizeFor(timeRange, interval time.Duration) int {
 	if interval.Nanoseconds() >= timeRange.Nanoseconds() {
 		return 1
 	}
